d5aa34f748b7d3948d4b9db859248bafb22620dc
dude <agyaw792@gmail.com>
2016-06-25 17:29:51 -0400

    create json to render

--- init.py
+++ init.py
@@ -1,45 +1,47 @@
 
-# from html.parser import HTMLParser
-# import urllib.request
-# #url = "https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md"
-# url = "https://raw.githubusercontent.com/weblancaster/awesome-IoT-hybrid/master/README.md"
-# with urllib.request.urlopen(url) as url:
-#     content = url.read().decode("utf-8")
-# mainHeaders = content.split('##')
-# print(len(mainHeaders))
+"""init module.
 
-# for mainHeader in mainHeaders:
-#     mainHeader = mainHeader.split('###')
-#     print (len(mainHeader))
-# print(len(mainHeaders))
+Parses main awesome list to extract child urls to be parsed. Dumps JSON to external text file once done.
 
-# print(type(content))
-# print(content)
-
-import json;
+Author: Ankit Gyawali (https://github.com/ankitgyawali).
+"""
+import parse
+import urllib.request
+import json
 import re
 
+# Parent URL
+url = "https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md"
+
+try:
+	content = urllib.request.urlopen(url).read().decode("utf-8")
+except:
+	print ("Failed to Parse Root URL : " +url)
+
+# Main Object that will hold all awesome data
 root = {}
 root['headers'] = []
 
-with open('readme.md', 'r') as content_file:
-    content = content_file.read()
-
+# Split by section markdown 
 mainHeaders = content.split('##')[2:-1]
 
+# Loop and parse all the sections
 for i, mainHeader in enumerate(mainHeaders):
 	mainHeaders[i] = mainHeader.splitlines()
 	if(mainHeader[i][0]!='License' and mainHeader[i][0]!='Contributors'):
 		head = {}
 		head['title'] = mainHeaders[i][0]
 		head['titleDetails'] = []
+		# Generate Name/URL for awesome lists
 		for j, headerLinks in enumerate(mainHeaders[i]):
 				details = {}
 				details['url'] = headerLinks.partition("(")[2].partition(")")[0]
 				details['name'] = headerLinks.partition("[")[2].partition("]")[0]
 				if(details['url']!='' and ('http' in details['url'])):
+					# IMPORTANT: This line does the parsing of all awesome URLS
+					#details['details'] = parse.parseUrl(details['url'])
 					head['titleDetails'].append(details)
 		root['headers'].append(head)
 
-print (root)
-
+with open('awesome.json', 'w') as outfile:
+    json.dump(root, outfile)
--- parse.py
+++ parse.py
@@ -1,65 +1,83 @@
+"""awesome list parser module.
+This module takes repository url of "awesome list" on github and parses it to return a JSON object. 
+Author: Ankit Gyawali (https://github.com/ankitgyawali).
+"""
+import urllib.request
 import json;
 import re
 
 
+def parseUrl(url):
+	# Modify URL to fetch raw data.
+	# TODO: Some URLs use 'readme.md' instead of 'README.md'. Try catch to use appropriate one.
+	url = url.replace("github", "raw.githubusercontent")+"/master/README.md"
 
+	# Awesome Object to return
+	awesome = {}
+	# Subheaders inside awesome list 
+	awesome['title'] =[]
 
-# url = "https://github.com/weblancaster/awesome-IoT-hybrid"
-# print (url.replace("github", "raw.githubusercontent")+"/master/README.md")
-# print ("https://raw.githubusercontent.com/weblancaster/awesome-IoT-hybrid/master/README.md")
-awesome = {}
-awesome['title'] =[]
+	# Check for 404, return if 404
+	try:
+		subcontent = urllib.request.urlopen(url).read().decode("utf-8")
+		print ("Parsed: " +url)
+	except:
+		print ("Failed: " +url)
+		return
 
-with open('2.md', 'r') as content_file:
-    content = content_file.read()
+	# Split by mainheader markdown "##"
+	mainHeaders = subcontent.split('\n## ')[1:-1]
 
+	# Split by subheader markdown "###" or "####"
+	for i, mainHeader in enumerate(mainHeaders): 
+		mainHeaders[i] = re.split('\n### |\n#### ',mainHeaders[i])
 
-mainHeaders = content.split('\n## ')[1:-1]
-
-
-
-for i, mainHeader in enumerate(mainHeaders): #Main BRANCH SPLIT BY ##
-	mainHeaders[i] = re.split('\n### |\n#### ',mainHeaders[i])
-
-	mainHeaders[i][0] = mainHeaders[i][0].splitlines()
-	section = {} # One section inside awesome list with no sub sections
-	section['name'] = mainHeaders[i][0][0]
-	section['links'] = []
-	section['subheaders'] = []
-	#print (mainHeaders[i][0])
-	for j, sectionLinks in enumerate(mainHeaders[i][0]): #PARSE 0th INDEX DIRECTLY BELOW MAIN HEADER
-		sectionDetails = {}
-		sectionDetails['url'] = sectionLinks.partition("(")[2].partition(")")[0]
-		sectionDetails['name'] = sectionLinks.partition("[")[2].partition("]")[0]
-		if(sectionDetails['url'].strip()!='' and ('http' in sectionDetails['url'])):
-			section['links'].append(sectionDetails)
-	
-	#IF HEADER HAS SUB HEADERS PARSE THEM TOO
-	mainHeaders[i].pop(0)
-	if (len(mainHeaders[i]) !=0): 
+		# Always parse first section of header
+		mainHeaders[i][0] = mainHeaders[i][0].splitlines()
+		section = {}
+		section['name'] = mainHeaders[i][0][0]
+		section['links'] = []
+		section['subheaders'] = []
+		# Begin parsing first section of header
+		for j, sectionLinks in enumerate(mainHeaders[i][0]): 
+			sectionDetails = {}
+			sectionDetails['url'] = sectionLinks.partition("(")[2].partition(")")[0]
+			sectionDetails['name'] = sectionLinks.partition("[")[2].partition("]")[0]
+			if(sectionDetails['url'].strip()!='' and ('http' in sectionDetails['url'])):
+				section['links'].append(sectionDetails)
+			# TODO: Debug
+			# else:
+			# 	if(sectionDetails['url'].strip()!='' and sectionDetails['url'][1]=="#"):
+			# 		print (sectionDetails['url'])
 		
 		
-		for k, subHeaders in enumerate(mainHeaders[i]): #Loop through subsection Heading
-			mainHeaders[i][k] = mainHeaders[i][k].splitlines()
-			subSection = {}
-			subSection['title'] = mainHeaders[i][k][0]
-			subSection['links'] = []
-			#print (mainHeaders[i][k][0])
-			for l, subHeadersSplit in enumerate(mainHeaders[i][k]): #Loop through one sub section
-				subSectionDetails = {}
-				subSectionDetails['url'] = subHeadersSplit.partition("(")[2].partition(")")[0]
-				subSectionDetails['name'] = subHeadersSplit.partition("[")[2].partition("]")[0]
-				#print (subSectionDetails)
-				# print (subSectionDetails['url'])
-				if(subSectionDetails['url'].strip()!='' and ('http' in subSectionDetails['url'])):
-					subSection['links'].append(subSectionDetails)
+		mainHeaders[i].pop(0)
+		# If header has subheaders parse all of them
+		if (len(mainHeaders[i]) !=0): 
+			# Loop through subsection Heading
+			for k, subHeaders in enumerate(mainHeaders[i]): 
+				mainHeaders[i][k] = mainHeaders[i][k].splitlines()
+				subSection = {}
+				subSection['title'] = mainHeaders[i][k][0]
+				subSection['links'] = []
+				# DEBUG: print (mainHeaders[i][k][0])
+				#Loop through one sub section
+				for l, subHeadersSplit in enumerate(mainHeaders[i][k]): 
+					subSectionDetails = {}
+					subSectionDetails['url'] = subHeadersSplit.partition("(")[2].partition(")")[0]
+					subSectionDetails['name'] = subHeadersSplit.partition("[")[2].partition("]")[0]
+					#print (subSectionDetails)
+					# print (subSectionDetails['url'])
+					if(subSectionDetails['url'].strip()!='' and ('http' in subSectionDetails['url'])):
+						subSection['links'].append(subSectionDetails)
 
-			#print (subSection)
-			section['subheaders'].append(subSection)
+				#print (subSection)
+				section['subheaders'].append(subSection)
+		# Get rid of empty arrays
+		if(len(section['links'])!=0):
+			if(len(section['subheaders'])==0):
+				del section['subheaders']
+			awesome['title'].append(section)
+	return awesome
 
-	if(len(section['links'])!=0):
-		if(len(section['subheaders'])==0):
-			del section['subheaders']
-		awesome['title'].append(section)
 
-print (awesome)
